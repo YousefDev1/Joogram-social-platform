e37e2994140eed694756ffea1ccf7e9c2601e628
Yousef A A <youesfabuaysa.dev@gmail.com>
2026-09-17 21:48:24 +0300

    After customize registration page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,15 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:50'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'username' => $request->username,
+            'avatar' => 'https://ui-avatars.com/api/?name='.urlencode($request->name),
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
@@ -46,6 +49,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('welcome'));
     }
 }
